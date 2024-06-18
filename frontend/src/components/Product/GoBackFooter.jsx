e35d748f0ce07977c0eb53e8f87f8752fafb3752
frfarhath <103945849+frfarhath@users.noreply.github.com>
2024-06-18 16:04:17 +0530

    Add files via upload

---
+++ frontend/src/components/Product/GoBackFooter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const GoBackFooter = ({ total, onNext, onBack }) => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="total-amount">Total: {total} LKR</div>
+        <div className="button-group">
+          <button onClick={onBack} className="footer-button">
+            Go Back
+          </button>
+          <button onClick={onNext} className="footer-button">
+            Next
+          </button>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default GoBackFooter;
