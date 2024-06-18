dc7313bdfd7e5eff6ee0040bef853b21b9c3dc98
frfarhath <103945849+frfarhath@users.noreply.github.com>
2024-06-18 16:05:12 +0530

    Add files via upload

---
+++ frontend/src/components/Product/ProductCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../css/product.css";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function ProductCard({
+  imgmehendi,
+  productname,
+  productdes,
+  price,
+  onViewProduct,
+}) {
+  return (
+    <div className="product-card">
+      <img src={imgmehendi} alt={productname} className="product-image" />
+      <p className="product-name">{productname}</p>
+      <p className="product-description">{productdes}</p>
+      <div className="product-price">LKR {price}</div>
+      <button className="product-button" onClick={onViewProduct}>
+        <FontAwesomeIcon icon={faEye} /> View Product
+      </button>
+    </div>
+  );
+}
