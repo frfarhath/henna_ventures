2910d98ea2a6304959fb389319260bbfe3278e73
Fathimafaisa Thaufeek <133674321+fasfaisa@users.noreply.github.com>
2024-06-23 15:10:51 +0530

    Delete nodejs_auth/src/domains/artist directory

--- nodejs_auth/src/domains/artist/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const artistController = require('./controller');
-
-router.post('/register', artistController.registerArtist);
-router.post('/approve/:artistId', artistController.approveArtist);
-router.post('/login', artistController.loginArtist);
-module.exports = router;
