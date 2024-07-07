1a0e9387185418ac6cadd483874696600ab691bc
frfarhath <103945849+frfarhath@users.noreply.github.com>
2024-07-07 23:35:25 +0530

    checkout

--- frontend/src/pages/Product/CheckoutInfo.jsx
+++ frontend/src/pages/Product/CheckoutInfo.jsx
@@ -10,50 +10,53 @@ export default function CheckoutInfo() {
     <div>
       <NewNav />
       <br />
-      <div className="message-container">
-        <div className="main-content">
+      <div className="message-containerr">
+        <div className="main-contentt">
           <h3>Reciever's Information</h3>
           <br />
-          <div className="column-2">
+          <div className="column-3">
             <div className="personalInfo">
               <div className="form-floating">
-                <label>Receiver Name:</label></div>
-                <br />
-                <input
-                  type="text"
-                  id="name"
-                  placeholder="Enter receiver's name"
-                  style={{ width: "50%" }}
-                />
+                <label>Receiver Name:</label>
               </div>
               <br />
-              <div className="form-floating">
-                <label>Delivery Address:</label></div>
-                <br />
-                <input
-                  type="text"
-                  id="address"
-                  placeholder="Enter receiver's address"
-                  style={{ width: "50%" }}
-                />
-              </div>
-              <br />
-              <div className="form-floating">
-                <label>Contact number:</label></div>
-                <br />
-                <input
-                  type="number"
-                  id="tel-number"
-                  placeholder="Enter receiver's name"
-                  style={{ width: "50%" }}
-                />
-             <br/>
-              <button className="save-button" onClick={handleButtonClick}>
-                Proceed for payment
-              </button>
+              <input
+                type="text"
+                id="name"
+                placeholder="Enter receiver's name"
+                style={{ width: "50%" }}
+              />
             </div>
+            <br />
+            <div className="form-floating">
+              <label>Delivery Address:</label>
             </div>
-          
+            <br />
+            <input
+              type="text"
+              id="address"
+              placeholder="Enter receiver's address"
+              style={{ width: "50%" }}
+            />
+          </div>
+          <br />
+          <div className="form-floating">
+            <label>Contact number:</label>
+          </div>
+          <br />
+          <input
+            type="number"
+            id="tel-number"
+            placeholder="Enter receiver's name"
+            style={{ width: "50%" }}
+          />
+          <br />
+          <button className="save-button" onClick={handleButtonClick}>
+            Proceed for payment
+          </button>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
