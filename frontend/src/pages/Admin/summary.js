39c913d13092226cdc9514e6f8a597151f8f5f30
AfRi99 <96653532+AfRi99@users.noreply.github.com>
2024-07-29 21:48:44 +0530

    Update summary.js

--- frontend/src/pages/Admin/summary.js
+++ frontend/src/pages/Admin/summary.js
@@ -7,9 +7,115 @@ import { BarChart } from '@mui/x-charts/BarChart';
 
 import SideBar from '../../components/Admin/sidebar';
 import Head from '../../components/Admin/head';
+import Loading from '../components/loading';
 
 
 class Summary extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            chartArray1: [],
+            loading: true,
+            totalOrders: 0,
+            totalPendings: 0,
+            totalDeliveries: 0,
+            totalAppoinments: 0,
+            productGraphArray: []
+        }
+
+    }
+
+    componentDidMount() {
+
+        const colors = ['#ff7c43', '#2f4b7c', '#665191', '#a05195', '#d45087', '#198754']
+
+        const fetchData = async () => {
+            try {
+                const res = await axios.get('http://localhost:8000/api/admin/getOrder');
+                const resdata = await res.data;
+
+                const subArray = [];
+                resdata.map((item) => {
+                    if (item.status === '1') {
+                        subArray.push({
+                            'title': item.product,
+                            'value': parseInt(item.quantity, 10)
+                        })
+                    }
+                });
+
+                this.setState({
+                    totalOrders: resdata.length,
+                    totalPendings: resdata.length - subArray.length,
+                    totalDeliveries: subArray.length,
+                });
+
+                const summarizeProducts = (subArray) => {
+                    const summary = subArray.reduce((acc, product) => {
+                        const { title, value } = product;
+                        if (!acc[title]) {
+                            acc[title] = { title, value: 0 };
+                        }
+                        acc[title].value += value;
+                        return acc;
+                    }, {});
+
+                    return Object.values(summary);
+                }
+
+                const finalSummary = summarizeProducts(subArray);
+
+                const withColors = (finalSummary, colors) => {
+                    return finalSummary.map((item, index) => ({
+                        ...item,
+                        color: colors[index % colors.length]
+                    }));
+                };
+                const piechartArray = withColors(finalSummary, colors);
+
+                this.setState({
+                    chartArray1: piechartArray,
+                    loading: false
+                });
+
+            } catch (error) {
+                console.log('Main Error', error);
+            }
+        };
+        fetchData();
+
+        const fetchData2 = async () => {
+            try {
+                const res1 = await axios.get('http://localhost:8000/api/admin/getConfirmAppoinmentPackage');
+                const res2 = await axios.get('http://localhost:8000/api/admin/getConfirmAppoinmentIndividual');
+                const res3 = await axios.get('http://localhost:8000/api/admin/getProduct');
+
+                const resdata1 = await res1.data;
+                const resdata2 = await res2.data;
+                const resdata3 = await res3.data;
+
+                const subArray = [];
+                resdata3.map((item) => {
+                    subArray.push({
+                        'title': item.name,
+                        'value': parseInt(item.count, 10)
+                    })
+                });
+
+                this.setState({
+                    totalAppoinments: resdata1.length + resdata2.length,
+                    productGraphArray: subArray
+                });
+
+            } catch (error) {
+                console.log('Main Error', error);
+            }
+        };
+        fetchData2();
+
+    }
 
 
     render() {
@@ -19,47 +125,6 @@ class Summary extends Component {
             height: 250,
         };
 
-        const dataset = [
-            {
-                seoul: 80,
-                month: 'Pro 6',
-            },
-            {
-                seoul: 131,
-                month: 'Pro 5',
-            },
-            {
-                seoul: 55,
-                month: 'Pro 4',
-            },
-            {
-                seoul: 48,
-                month: 'Pro 3',
-            },
-            {
-                seoul: 25,
-                month: 'Pro 2',
-            },
-            {
-                seoul: 60,
-                month: 'Pro 1',
-            },
-
-        ];
-
-        const data = [
-            { title: 'Product 1', value: 10, color: '#ff7c43' },
-            { title: 'Product 2', value: 15, color: '#2f4b7c' },
-            { title: 'Product 3', value: 20, color: '#665191' },
-            { title: 'Product 4', value: 5, color: '#a05195' },
-            { title: 'Product 5', value: 8, color: '#d45087' },
-            { title: 'Product 6', value: 13, color: '#198754' },
-        ];
-
-        // const totalValue = data.reduce((acc, cur) => acc + cur.value, 0);
-
-
-
         return (
             <div className='body'>
                 <SideBar />
@@ -68,143 +133,127 @@ class Summary extends Component {
 
                     <div className='conbody'>
 
-                        <div className='producthead'>
-                            <h3 className='productheadtxt'>SUMMARY</h3>
-                        </div>
+                        {!this.state.loading && (
+                            <div>
 
-                        <div style={{ display: 'flex' }}>
+                                <div className='producthead'>
+                                    <h3 className='productheadtxt'>SUMMARY</h3>
+                                </div>
 
-                            <div style={{ width: '40%', marginLeft: '1rem' }}>
-                                <h2 style={{ color: '#804f0e', textAlign: 'center' }}>TOP SELLING ITEMS</h2>
+                                <div style={{ display: 'flex' }}>
 
-                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <div style={{ width: '40%', marginLeft: '1rem' }}>
+                                        <h2 style={{ color: '#804f0e', textAlign: 'center' }}>TOP SELLING ITEMS</h2>
 
-                                    <div style={{ display: 'flex', flexDirection: 'column', marginTop: 30 }}>
-                                        {data.map((entry, index) => (
-                                            <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
-                                                <div
-                                                    style={{
-                                                        width: '15px',
-                                                        height: '15px',
-                                                        backgroundColor: entry.color,
-                                                        marginRight: '10px',
-                                                        borderRadius: '15px'
-                                                    }}
-                                                />
-                                                <span style={{ fontSize: '13px' }}>{entry.title} - {entry.value}</span>
+                                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+
+                                            <div style={{ display: 'flex', flexDirection: 'column', marginTop: 30 }}>
+                                                {this.state.chartArray1.map((entry, index) => (
+                                                    <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
+                                                        <div
+                                                            style={{
+                                                                width: '15px',
+                                                                height: '15px',
+                                                                backgroundColor: entry.color,
+                                                                marginRight: '10px',
+                                                                borderRadius: '15px'
+                                                            }}
+                                                        />
+                                                        <span style={{ fontSize: '13px' }}>{entry.title} - {entry.value}</span>
+                                                    </div>
+                                                ))}
                                             </div>
-                                        ))}
+
+                                            <div style={{ width: '270px', height: '240px' }}>
+                                                <PieChart
+                                                    data={this.state.chartArray1}
+                                                    label={({ dataEntry }) => `${Math.round(dataEntry.value)}`}
+                                                    labelStyle={{
+                                                        fontSize: '5px',
+                                                        fontFamily: 'sans-serif',
+                                                        fill: 'white',
+                                                    }}
+                                                    labelPosition={80}
+                                                />
+                                            </div>
+
+                                        </div>
+
                                     </div>
 
-                                    <div style={{ width: '270px', height: '240px' }}>
-                                        <PieChart
-                                            data={data}
-                                            label={({ dataEntry }) => `${Math.round(dataEntry.value)}`}
-                                            labelStyle={{
-                                                fontSize: '5px',
-                                                fontFamily: 'sans-serif',
-                                                fill: 'white',
-                                            }}
-                                            labelPosition={80}
+                                    <div style={{ width: '50%', marginLeft: '3rem' }}>
+                                        <h2 style={{ color: '#804f0e', textAlign: 'center' }}>INVENTORY</h2>
+
+                                        <BarChart
+                                            dataset={this.state.productGraphArray}
+                                            yAxis={[{ scaleType: 'band', dataKey: 'title' }]}
+                                            series={[{
+                                                dataKey: 'value',
+                                            }]}
+                                            layout="horizontal"
+                                            {...chartSetting}
+                                            margin={{ top: 5, left: 80}}
                                         />
+
+                                    </div>
+
+                                </div>
+
+
+                                <h2 style={{ color: '#804f0e', textAlign: 'center', marginBottom: 0 }}>SALES ORDER</h2>
+                                <div className='row1' style={{ marginBottom: '2rem' }}>
+
+                                    <div className='card'>
+                                        <div className='itemsum'>
+                                            <h4 className='item1txt'>ORDERS</h4>
+                                        </div>
+                                        <div className='item'>
+                                            <h1 className='itemtxtsum'>{this.state.totalOrders}</h1>
+                                        </div>
+                                    </div>
+
+                                    <div className='card'>
+                                        <div className='itemsum'>
+                                            <h4 className='item1txt'>PENDING</h4>
+                                        </div>
+                                        <div className='item'>
+                                            <h1 className='itemtxtsum'>{this.state.totalPendings}</h1>
+                                        </div>
+                                    </div>
+
+                                    <div className='card'>
+                                        <div className='itemsum'>
+                                            <h4 className='item1txt'>DELIVERED</h4>
+                                        </div>
+                                        <div className='item'>
+                                            <h1 className='itemtxtsum'>{this.state.totalDeliveries}</h1>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <h2 style={{ color: '#804f0e', textAlign: 'center', marginBottom: 0 }}>WEEKLY APPOINMENT REPORT</h2>
+                                <div className='row1' style={{ marginBottom: '2rem', display: 'flex', justifyContent: 'center' }}>
+
+                                    <div className='card'>
+                                        <div className='itemsum' style={{ backgroundColor: 'green' }}>
+                                            <h4 className='item1txt'>APPOINMENTS</h4>
+                                        </div>
+                                        <div className='item'>
+                                            <h1 className='itemtxtsum'>{this.state.totalAppoinments}</h1>
+                                        </div>
                                     </div>
 
                                 </div>
 
                             </div>
+                        )}
 
-                            <div style={{ width: '50%', marginLeft: '3rem' }}>
-                                <h2 style={{ color: '#804f0e', textAlign: 'center' }}>INVENTORY</h2>
-
-                                <BarChart
-                                    dataset={dataset}
-                                    yAxis={[{ scaleType: 'band', dataKey: 'month' }]}
-                                    series={[{
-                                        dataKey: 'seoul',
-                                    }]}
-                                    layout="horizontal"
-                                    {...chartSetting}
-                                    margin={{ top: 5 }}
-                                />
-
+                        {this.state.loading && (
+                            <div style={{ marginTop: '2rem' }}>
+                                <Loading />
                             </div>
-
-                        </div>
-
-
-                        <h2 style={{ color: '#804f0e', textAlign: 'center', marginBottom: 0 }}>SALES ORDER</h2>
-                        <div className='row1' style={{ marginBottom: '2rem' }}>
-
-                            <div className='card'>
-                                <div className='itemsum'>
-                                    <h4 className='item1txt'>ORDERS</h4>
-                                </div>
-                                <div className='item'>
-                                    <h1 className='itemtxtsum'>28</h1>
-                                </div>
-                            </div>
-
-                            <div className='card'>
-                                <div className='itemsum'>
-                                    <h4 className='item1txt'>PENDING</h4>
-                                </div>
-                                <div className='item'>
-                                    <h1 className='itemtxtsum'>23</h1>
-                                </div>
-                            </div>
-
-                            <div className='card'>
-                                <div className='itemsum'>
-                                    <h4 className='item1txt'>DELIVERED</h4>
-                                </div>
-                                <div className='item'>
-                                    <h1 className='itemtxtsum'>05</h1>
-                                </div>
-                            </div>
-
-                        </div>
-
-                        <h2 style={{ color: '#804f0e', textAlign: 'center', marginBottom: 0 }}>WEEKLY APPOINMENT REPORT</h2>
-                        <div className='row1' style={{ marginBottom: '2rem', display: 'flex', justifyContent: 'center' }}>
-
-                            <div className='card'>
-                                <div className='itemsum' style={{ backgroundColor: 'green' }}>
-                                    <h4 className='item1txt'>APPOINMENTS</h4>
-                                </div>
-                                <div className='item'>
-                                    <h1 className='itemtxtsum'>20</h1>
-                                </div>
-                            </div>
-
-                        </div>
-
-                        <div style={{marginRight: '1rem'}}>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>NAME</th>
-                                        <th>LOCATION</th>
-                                        <th>DATE</th>
-                                        <th>TIME</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Malini</td>
-                                        <td>malini@Colombo.com</td>
-                                        <td>01/01/2024</td>
-                                        <td>1.00 pm</td>
-                                    </tr>
-                                    <tr>
-                                        <td>John</td>
-                                        <td>malini@Colombo.com</td>
-                                        <td>02/02/2024</td>
-                                        <td>3.00 pm</td>
-                                    </tr>
-
-                                </tbody>
-                            </table>
-                        </div>
+                        )}
 
                     </div>
 
