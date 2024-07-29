9156860eea2bd5514bcc80968858ccad0c016770
AfRi99 <96653532+AfRi99@users.noreply.github.com>
2024-07-29 21:31:09 +0530

    Update artist.js

--- frontend/src/pages/Admin/artist.js
+++ frontend/src/pages/Admin/artist.js
@@ -9,6 +9,8 @@ import Head from '../../components/Admin/head';
 
 import AcceptArtist from '../../modals/acceptArtist';
 
+import Loading from '../components/loading';
+
 class Artist extends Component {
 
     constructor(props) {
@@ -16,17 +18,17 @@ class Artist extends Component {
 
         this.state = {
             show: false,
-            artists: [
-                { id: 1, name: "Dhana", phone: "011-7426984", email: "dhana@gmail.com", location: "Colombo", status: null }
-                // You can add more initial artists here
-            ]
+            fetchArray: [],
+            loading: true,
+
+            passingArray: {}
         }
     }
 
-
-    showModal = () => {
+    showModal = (item) => {
         this.setState({
-            show: true
+            show: true,
+            passingArray: { ...item }
         });
     }
 
@@ -36,7 +38,47 @@ class Artist extends Component {
         });
     }
 
+    componentDidMount() {
+
+        const fetchData = async () => {
+            try {
+                const res = await axios.get('http://localhost:8000/api/v1/admin/getArtist');
+                const resdata = await res.data;
+
+                this.setState({
+                    fetchArray: resdata,
+                    loading: false
+                });
+
+            } catch (error) {
+                console.log('Main Error', error);
+            }
+        };
+        fetchData();
+
+    }
+
+    handleDlete = async (id) => {
+
+        try {
+
+            const res = await axios.delete(`http://localhost:8000/api/v1/admin/deleteArtist/${id}`);
+
+            const resdata = await res.data;
+            console.log(resdata);
+            alert('Successfully !')
+            window.location.reload();
+
+        } catch (error) {
+            console.log('Main Error', error);
+            alert('Failed !')
+        }
+
+    };
+
+
     render() {
+
         return (
             <div className='body'>
 
@@ -49,61 +91,73 @@ class Artist extends Component {
                             <h3 className='productheadtxt'>ARTISTS</h3>
                         </div>
                         <div className='producttable'>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>FULLNAME</th>
-                                        <th>PHONE</th>
-                                        <th>EMAIL</th>
-                                        <th>LOCATION</th>
-                                        <th>PREVIOUS WORK</th>
-                                        <th>E-CERTIFICATE</th>
-                                        <th>ACTIONS</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {this.state.artists.map(artist => (
-                                        <tr key={artist.id}>
-                                            <td>{artist.name}</td>
-                                            <td>{artist.phone}</td>
-                                            <td>{artist.email}</td>
-                                            <td>{artist.location}</td>
-                                            <td style={{ textAlign: 'center', color: 'blue', padding: 0 }}>
-                                                Download
-                                            </td>
-                                            <td style={{ textAlign: 'center', color: 'blue', padding: 0 }}>
-                                                Download
-                                            </td>
-                                            <td style={{ justifyContent: 'center', display: 'flex' }}>
-                                                <FaCheck
-                                                    size={22}
-                                                    style={{ marginRight: 5, color: 'green', cursor: 'pointer' }}
-                                                    className='FaCheck'
-                                                    onClick={() => this.showModal()}
-                                                />
-                                                <FaTimes
-                                                    size={22}
-                                                    style={{ color: 'red', cursor: 'pointer' }}
-                                                    className='FaTimes'
-                                                />
-                                            </td>
+
+                            {!this.state.loading && (
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>FULLNAME</th>
+                                            <th>PHONE</th>
+                                            <th>EMAIL</th>
+                                            <th>LOCATION</th>
+                                            <th>PREVIOUS WORK</th>
+                                            <th>E-CERTIFICATE</th>
+                                            <th>ACTIONS</th>
                                         </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                                    </thead>
+
+                                    <tbody>
+
+                                        {this.state.fetchArray.map((item, index) => (
+
+                                            <tr key={index}>
+                                                <td>{item.full_name}</td>
+                                                <td>{item.phone}</td>
+                                                <td>{item.email}</td>
+                                                <td>{item.location}</td>
+                                                <td style={{ textAlign: 'center', color: 'blue', padding: 0 }}>
+                                                    Download
+                                                </td>
+                                                <td style={{ textAlign: 'center', color: 'blue', padding: 0 }}>
+                                                    Download
+                                                </td>
+                                                <td style={{ justifyContent: 'center', display: 'flex' }}>
+                                                    <FaCheck
+                                                        size={22}
+                                                        style={{ marginRight: 5, color: 'green', cursor: 'pointer' }}
+                                                        className='FaCheck'
+                                                        onClick={() => this.showModal(item)}
+                                                    />
+                                                    <FaTimes
+                                                        size={22}
+                                                        style={{ color: 'red', cursor: 'pointer' }}
+                                                        className='FaTimes'
+                                                        onClick={() => this.handleDlete(item._id)}
+                                                    />
+                                                </td>
+                                            </tr>
+
+                                        ))}
+                                    </tbody>
+
+                                </table>
+                            )}
+
+                            {this.state.loading && (
+                                <div style={{ marginTop: '2rem' }}>
+                                    <Loading />
+                                </div>
+                            )}
+
                         </div>
                     </div>
                 </div>
 
-                {/* Modal */}
-                <AcceptArtist
-                    show={this.state.show}
-                    handleClose={this.hideModal}
-                />
-
+                <AcceptArtist show={this.state.show} handleClose={this.hideModal} passing={this.state.passingArray} />
 
             </div>
         )
+
     }
 }
 
